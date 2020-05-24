e6ded9f4d4c92bf43c44ff1bcbcbfc0d9ca19a1c
RikGr <ragroenewoud@gmail.com>
2020-05-24 15:13:26 +0200

    google maps fixed

--- public/main.js
+++ public/main.js
@@ -1,7 +1,10 @@
-let map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
+// Initialize and add the map
+function initMap() {
+    // The locations
+    var balkan = {lat: 41.0052367, lng: 28.8720967};
+    // The map, centered at Balkan
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 6, center: balkan});
+    // The marker, positioned at Balkan
+    var marker = new google.maps.Marker({position: balkan, map: map});
+  }
