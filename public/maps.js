51cf9bd031134773c1f413e8406227dd24eb054e
RikGr <ragroenewoud@gmail.com>
2020-06-01 14:01:58 +0200

    photo gallery removed - infobox in

--- public/maps.js
+++ public/maps.js
@@ -1,16 +1,22 @@
+
+
+
 function initMap() {
-
-
 let locations = [
-    ['<a href="https://www.wikipedia.com/wiki/Constantinople">Constantinople</a>', 41.0052367, 28.8720967, 3, "#photoCon_constantinople"],
-    ['<a href="https://www.wikipedia.com/wiki/Nicopolis_ad_Istrum">Nicopolis Ad Istrum</a>', 41.595881, 23.7949809, 2,"#photoCon_NicopolisAdIstrum"],
-    ['<a href="https://www.wikipedia.com/wiki/Dorostorum">Dorostorum</a>', 44.0462107, 27.1737668, 1,"#photoCon_dorostorum"],
+    ['<a href="https://www.wikipedia.com/wiki/Constantinople" target="_blank">Constantinople</a>', 41.0052367, 28.8720967, 3,
+    '<img src="https://i1.wp.com/www.mikepole.com/wp-content/uploads/2016/10/Front-105635.jpg" >'],
+   
+    ['<a href="https://www.wikipedia.com/wiki/Nicopolis_ad_Istrum" target="_blank">Nicopolis Ad Istrum</a>', 41.595881, 23.7949809, 2,
+    '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Nicopolis_ad_Istrum_013.jpg/800px-Nicopolis_ad_Istrum_013.jpg" >'],
+
+    ['<a href="https://www.wikipedia.com/wiki/Dorostorum" target="_blank">Dorostorum</a>', 44.0462107, 27.1737668, 1,
+    '<img src="https://upload.wikimedia.org/wikipedia/commons/3/33/Silistra_River_Danube.jpg" >'  ],
   ];
 
  let map = new google.maps.Map(document.getElementById('map'), {
     zoom: 6,
     center: new google.maps.LatLng(42.00, 26.00),
-    mapTypeId: google.maps.MapTypeId.HYBRID
+    /* mapTypeId: google.maps.MapTypeId.HYBRID */
     });
 
   let infowindow = new google.maps.InfoWindow();
@@ -21,16 +27,13 @@ let locations = [
       position: new google.maps.LatLng(locations[i][1], locations[i][2]),
       map: map
     });
-    google.maps.event.addListener(marker, 'click', (function (marker, i) {
+    google.maps.event.addListener(marker, 'click', (function createInfoWindow (marker, i) {
       return function() {
-        $(locations[i][4]).toggle("slow");
-         
-        let scrolled=0;      
-       scrolled= scrolled+300;  
+        infowindow.setContent(locations[i][0] + locations[i][4]);
+        infowindow.open(map, marker);
       }
     })
-      (marker, i));
-  
-  }
+    (marker, i));
+  } 
 }  
 
